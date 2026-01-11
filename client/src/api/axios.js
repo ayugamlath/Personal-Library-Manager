c9535f99105d63e5d3904aeef619101566c63a38
Achintha Gamlath <augamlath@gmail.com>
2026-01-11 13:51:34 +0530

    vercel build configurations

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "/api",
 });
 const book_api = axios.create({
   baseURL: "https://www.googleapis.com/books/v1",
